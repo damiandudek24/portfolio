f2b71eef598a4016205b16cc4076ff85016bc9f8
LAPTOP-IPS775OC\Acer <damdud4776@gmail.com>
2023-03-14 12:42:05 +0100

    Help added with basic pass

--- index.php
+++ index.php
@@ -57,9 +57,11 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     
     <title>Logowanie</title>
 </head>
+
 
 <body>
     <section id="top_sec">
@@ -90,12 +92,46 @@
                 <br><br>
                 <button type="submit" class="add_btn">Zaloguj</button>
             </form>
+            <button onclick="myFunction()">
+                <span class="material-symbols-outlined">
+                    live_help
+                </span>
+            </button>
+
+            <div id="pass_help" style="width: 200px;
+                padding: 12px 20px;
+                margin: 8px 0;
+                display: none;
+                border: 1px solid #ccc;
+                border-radius: 10px;
+                box-sizing: border-box;
+                color: #41403e
+                top: 0px;
+                left: 0px;
+                font-size: 16px;
+                background-color: #fff;	
+                transition: all 0.5s ease-in-out;
+                ">
+                Login: damian <br> hasło: 1234
+            </div>
         </center>
         </section>
     </section>
-
+    <br><br>
     <footer class="footer">
             <center><p>Damian Dudek</p></center>
     </footer>
+
+    <script>
+        function myFunction() 
+        {
+            var x = document.getElementById("pass_help");
+            if (x.style.display === "none") {
+                x.style.display = "block";
+            } 
+            else 
+                x.style.display = "none";
+        }
+</script>
 </body>
 </html>
